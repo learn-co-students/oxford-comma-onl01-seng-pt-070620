706e6720e1733aa95436b6967f3d35f1cccd8534
Cristian Deleon <libaration@gmail.com>
2020-07-07 14:17:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+    case array.length
+    when 1
+      array.join()
+    when 1..2
+    array.join(' and ')
 
+    when 3
+      ##myVariable = array[0] + ", " + array[1] + " and " + array[2]
+    ##  puts myVariable
+    myVar = array[0..1].join(", ")
+    myVar << ", and " << array[2]
+  else
+    myVar = array[0..array.size - 2].join(", ") ## I know there has to be a better way of doing this.
+    myVar << ", and " << array[array.size - 1]
+  end
 end
+##myArray = %w[dog cat mouse bear]
+##oxford_comma(myArray)
