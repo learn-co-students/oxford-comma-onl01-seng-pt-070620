6eebe1bccb49c984e9461cee7ac13dbf140465e3
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-08 23:08:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count >= 3
+    array.join(' and ')
+    array[-1] = "and " + array[-1]
+    array.join(', ')
+  end
 end
