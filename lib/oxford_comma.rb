90f20a2ae8d40b3ec08446ca240a580899baa4c5
Jacob Viti <thevitij64@gmail.com>
2020-07-10 14:49:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+case array.length
+when 1
+  return array.join
+when 2
+  return array.join(" and ")
+else
+  lastItem = array[-1]
+  array.pop
+  oxford = array.join(", ")
+  return oxford + ", and " + lastItem
+end
 end
