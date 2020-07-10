3706ec897b9d735ae004c92f23c2d466d5c2769e
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-10 23:03:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  else array.push[-1] = "and #{array[-1]}"
+  else array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
 end
