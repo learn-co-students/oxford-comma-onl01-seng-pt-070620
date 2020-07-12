86b90ad66d9c8051389045c8e8995ec358b082dd
github username <jhernandez1692@gmail.com>
2020-07-12 03:48:37 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+if  array.count == 1 
+   return array[0]
+elsif array.count == 2
+    array.map {|i| %Q(#{i}) }.join(' and ')
+elsif array.count == 3
+  array[0..-2].join(", ") + ", and " + array[-1]
+else
+    array[0..-2].join(", ") + ", and " + array[-1]  
+
+end
 
 end
