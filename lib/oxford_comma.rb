0b896c031d14fceb8f0d4bf1edc1eb8e5b321d77
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-13 23:45:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+def oxford_comma(array) 
+if array.length == 1 
+  array[0]
+elsif array.length == 2 
+  array.join(" and ")
+else 
+  array[-1].insert(0,"and ")
+  array.join(", ")
 
-end
+end 
+
+ end
+ 
+
+
+
+
+
