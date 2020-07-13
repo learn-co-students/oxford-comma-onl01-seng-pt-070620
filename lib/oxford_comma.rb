ff82021fad97e798f97cf19294aa8674f8ff7776
Antonio James <asjames18@gmail.com>
2020-07-13 15:07:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+if array.length == 2
+  return array.join(" and ")
+elsif array.length >= 3
+array.last.insert(0, "and ")
+ end
+ array.join(", ")
+
+#  if  (#{""})
+#   puts ("kiwi")
+# # elsif "2"
+# #   puts array.join (" and ")
+# # elsif array.length = 3
+# #   puts "kiwi, durian, and starfruit"
+# # else array.length >= 3
+# #   puts
+
 end
