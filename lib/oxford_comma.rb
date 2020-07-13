80107dc67fe432cee4b8ef71712015aceb57fcc4
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-13 03:33:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+
 def oxford_comma(array)
+  
+  if array.length == 1 
+    return array.join()
+  end
+  
+  if array.length == 2
+    return array.join(" and ")
+  end
+
+  if array.length >= 3 
+    element = array.pop
+    array << "and #{element}"
+    return array.join(", ")
+  end
 
 end
