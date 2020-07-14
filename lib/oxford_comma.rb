84ec66e45bce150d3c2c4a0fa9d9d095e8126b90
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-14 03:50:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ case array.length
+  when 1
+   array.join()
+  when 1..2
+    array.join(' and ')
+   else
+     array[-1].insert(0, 'and ')
+     array.join(', ')
+  end
 end
