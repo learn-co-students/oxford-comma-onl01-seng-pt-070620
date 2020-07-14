292b3b61d87e9bfc10da7b94a3b64275da78078b
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-14 05:05:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1 
+    return array.join
+  when 2 
+    return array.join(" and ")
+  else 
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
