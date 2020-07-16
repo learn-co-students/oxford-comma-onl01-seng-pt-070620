4943c9881e39c07f9a90b324c949b5f05ec60cee
Victor Avila <avilavictor1570@gmail.com>
2020-07-16 04:13:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.first
+  elsif array.size == 2 
+    array.insert(1, "and").join(" ")
+  elsif array.size > 2
+    array[0..-2].join(", ") + ", and #{array [-1]}"
+  end
 end
