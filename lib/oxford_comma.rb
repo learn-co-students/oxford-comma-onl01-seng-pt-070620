fc2c8b49a48c10ff9d32a62503b4df22ff71070e
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-16 06:38:54 -0400

    add comment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
+# ["kiwi", "durian", "starfruit"]  =>  "kiwi, durian, and starfruit"
+
 def oxford_comma(array)
   if array.length == 1
-    list_with_oxford_comma=array.join
+    list_with_oxford_comma = array.join
   elsif array.length == 2
-    list_with_oxford_comma=array.join(" and ")
+    list_with_oxford_comma = array.join(" and ")
   else
     last_item = array.pop
-    list_with_oxford_comma=array.join(", ")
+    list_with_oxford_comma = array.join(", ")
     list_with_oxford_comma << ", and " + last_item
   end
 end
